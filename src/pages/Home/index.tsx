42b6766c945a73437b3ff3ee08a445f3d741e82f
Renato de Toledo Machado <renatodtm@bol.com.br>
2021-05-23 21:28:19 -0300

    Project finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,15 +5,27 @@ import './styles.scss';
 
 const Home = () => (
     <div className="col-6 home-container">
-        
-        <h1 className="text-title">Desafio do Capítulo 3,<br/> Bootcamp DevSuperior</h1>
-        <p className="text-subtitle">Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.<br/><br/>
-            Favor observar as instruções passadas no capítulo sobre a elaboração deste projeto.<br/><br/>
-            Este design foi adaptado a partir de Ant Design System for Figma, de Mateusz Wierzbicki: <a href="mailto:antforfigma@gmail.com">antforfigma@gmail.com</a></p>   
+
+        <h1 className="text-title">
+            Desafio do Capítulo 3,<br /> Bootcamp DevSuperior
+        </h1>
+        <ul className="text-subtitle">
+            <li >
+                Bem-vindos a o desafio do capitulo 3 do Bootcamp DevSuperior.<br/><br/>
+            </li>
+            <li>
+                Favor observar as instruções passadas no capitulo sobre a <br/> elaboração deste projeto.<br/><br/>
+            </li>
+            <li>
+                Este design foi adaptado a partir de Ant Design System for Figma, de <br/>Mateusz Wierzbicki:
+                        <a href="/"> antforfigma@gmail.com</a>
+            </li>
+        </ul>
+
         <Link to="/search">
             <Button text="Começar" />
         </Link>
-        
+
     </div>
 );
 
